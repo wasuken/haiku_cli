75b5386f2978d2a8fa39a91923fbde348fb4c053
wasuken <wevorence@gmail.com>
2019-05-13 11:55:14 +0900

    adjust indent.

--- main.go
+++ main.go
@@ -16,23 +16,22 @@ func main() {
 	for _, arg := range os.Args[1:] {
 		height += len(arg) - 1
 		haiku_ary[haiku_count-width-1] = haiku_space + arg
-		for i := 0; i < len(arg)/2; i++ {
+		for i := 0; i < len(arg)/3-1; i++ {
 			haiku_space += " "
 		}
 		width++
 	}
 	for i := 0; i < len(haiku_ary[0]); i++ {
 		for j := 0; j < len(haiku_ary); j++ {
-			// fmt.Println(haiku_ary[j])
 			if i < len([]rune(haiku_ary[j])) {
-				// fmt.Print(haiku_ary[j][i])
-				// fmt.Print(haiku_ary[j][i])
-				// fmt.Print([]rune(haiku_ary[j])[i])
+				if j < len(haiku_ary)-2 {
+					fmt.Print("  ")
+				}
 				fmt.Print(string([]rune(haiku_ary[j])[i]))
-			} else {
 				fmt.Print("  ")
+			} else {
+				fmt.Print("")
 			}
-
 		}
 		fmt.Println()
 	}
